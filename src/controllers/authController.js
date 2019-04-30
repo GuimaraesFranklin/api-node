cf38ccc285941e3fd7aac5c34462058c5f8a3bf5
Franklin Guimaraes <franklincassiano@gmail.com>
2019-04-30 10:40:46 -0300

    first commit api-node

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -5,17 +5,31 @@ const User = require('../models/user');
 const router = express.Router();
 
 router.post('/register', async (req, res) => {
-
+    const { email } = req.body;
     try {
+
+        if (await User.findOne({
+            email
+        }))
+            return res.status(400).send({
+                error: 'User already exists '
+            });
+
 
         const user = await User.create(req.body);
 
-        return res.send({ user });
+        user.password = undefined;
+
+        return res.send({
+            user
+        });
 
     } catch (err) {
-        
-        return res.status(400).send({ error: 'Registration failed' });
+
+        return res.status(400).send({
+            error: 'Registration failed'
+        });
     }
 });
 
-module.exports = app = app.use('/auth', router);
+module.exports = app => app.use('/auth', router);
